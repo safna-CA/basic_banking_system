e1ea3110b343e64a1a5e02f941e9a5ff47c69609
safna-CA <67093814+safna-CA@users.noreply.github.com>
2021-07-20 01:52:08 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,28 @@
+     
+     <link href="css/nav-style.css" rel="stylesheet" type="text/css">
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      <nav class=" navbar navbar-expand-lg navbar navbar-dark bg-dark">
+        <img src="images/images.png" alt="Italian Trulli "style="width:70px;height:60px;" class="rounded">
+      <br>
+      <a class="navbar-brand" href="index.php" style="color : white;">  The Sparks Bank</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-333"
+          aria-controls="navbarSupportedContent-333" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent-333">
+          <ul class="navbar-nav ml-auto nav-flex-icons">
+            <li class="nav-item">
+            <a class="btn btn-outline-success" href="index.php"><i class="fa fa-home"></i>Home</i></a>
+            </li>
+            <li class="nav-item">
+              <a class="btn btn-outline-primary" href="transfermoney.php"><i class="fa fa-user-circle-o fa-lg">Clients</i></a>
+              </a>
+            </li>
+            
+            <li class="nav-item">
+                <a class="btn btn-outline-danger" href="transaction.php"><i class="fa fa-list-alt">Transactions</i></a>
+                </li>
+            
+          </ul>
+        </div>
+      </nav>
